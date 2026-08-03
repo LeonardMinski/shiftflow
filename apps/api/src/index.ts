979113ba6b104a966e9ee4af8e3aced0752f218f
Leonard Minski <your-github-email@example.com>
2026-08-03 11:29:06 +0100

    feat(api): add create employee mutation

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,12 @@
+console.log("index loaded");
+
 import { startServer } from "./server.js";
+
+console.log("calling startServer");
 
 startServer().catch((error: unknown) => {
   console.error("Failed to start GraphQL API", error);
   process.exit(1);
 });
+
+console.log("index.ts is executing");
